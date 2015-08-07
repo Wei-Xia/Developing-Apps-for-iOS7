31d6c95052745dff03ed4b35ad2b2aa22e7d681d
Wei Xia <xw900812@gmail.com>
2015-08-06 23:48:38 -0400

    Lecture 1

---
+++ cardGame/cardGame/Card.h
@@ -0,0 +1,13 @@
+//
+//  Card.h
+//  cardGame
+//
+//  Created by Wei on 8/6/15.
+//  Copyright (c) 2015 Gate Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Card : NSObject
+
+@end
---
+++ cardGame/cardGame/Card.m
@@ -0,0 +1,13 @@
+//
+//  Card.m
+//  cardGame
+//
+//  Created by Wei on 8/6/15.
+//  Copyright (c) 2015 Gate Inc. All rights reserved.
+//
+
+#import "Card.h"
+
+@implementation Card
+
+@end
