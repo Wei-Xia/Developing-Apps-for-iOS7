e37f3a18020a7b0f693b61869aaca4eeb4579bf1
Wei Xia <xw900812@gmail.com>
2015-08-08 18:00:30 -0400

    Lecture 2

--- cardGame/cardGame/Deck.m
+++ cardGame/cardGame/Deck.m
@@ -14,12 +14,19 @@
 
 @implementation Deck
 
+- (NSMutableArray *)cards
+{
+    if (!_cards)
+        _cards = [[NSMutableArray alloc] init];
+    return _cards;
+}
+
 - (void)addCard:(Card *)card atTop:(BOOL)atTop
 {
     if (atTop) {
         [self.cards insertObject:card atIndex:0];
     } else {
-        [self addCard:card atTop:NO];
+        [self.cards addObject:card];
     }
 }
 
@@ -28,6 +35,15 @@
     [self addCard:card atTop:NO];
 }
 
-- (Card *)drawRandomCard {}
+- (Card *)drawRandomCard
+{
+    Card *randomCard = nil;
+    
+    unsigned index = arc4random() % [self.cards count];
+    randomCard = self.cards[index];
+    [self.cards removeObjectAtIndex:index];
+    
+    return randomCard;
+}
 
 @end
