f4a3668c3e90698314129fe1780e732300b36dec
Wei Xia <xw900812@gmail.com>
2015-08-08 18:54:31 -0400

    Lecture 2

---
+++ Matchismo/Matchismo/CardGameViewController.h
@@ -0,0 +1,13 @@
+//
+//  CardGameViewController.h
+//  Matchismo
+//
+//  Created by Wei on 8/8/15.
+//  Copyright (c) 2015 CS193p. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CardGameViewController : UIViewController
+
+@end
