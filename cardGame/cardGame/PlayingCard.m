d8665a44002c0ff016b3aeba8e817aa4a29e5e27
Wei Xia <xw900812@gmail.com>
2015-08-08 18:45:06 -0400

    Lecture 2

--- cardGame/cardGame/PlayingCard.m
+++ cardGame/cardGame/PlayingCard.m
@@ -12,8 +12,26 @@
 
 - (NSString *)contents
 {
-    NSArray *rankStrings = @[@"?",@"A",@"2",@"3",...,@"10",@"J",@"Q",@"K"];
+    NSArray *rankStrings = @[@"?",@"A",@"2",@"3",@"4",@"5",@"6",@"7",@"8",@"9",@"10",@"J",@"Q",@"K"];
     return [rankStrings[self.rank] stringByAppendingString:self.suit];
 }
 
+@synthesize suit = _suit;
+
++ (NSArray *)validSuits
+{
+    return @[@"♥",@"♦",@"♠",@"♣"];
+}
+
+- (void)setSuit:(NSString *)suit
+{
+    if ([[PlayingCard validSuits] containsObject:suit]) {
+        _suit = suit;
+    }
+}
+
+- (NSString *)suit
+{
+    return  _suit ? _suit : @"?";
+}
 @end
