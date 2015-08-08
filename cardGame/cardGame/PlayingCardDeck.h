865daf352b0bfe33e6a61aea8ceea362c9857763
Wei Xia <xw900812@gmail.com>
2015-08-08 18:53:04 -0400

    Lecture 2

---
+++ cardGame/cardGame/PlayingCardDeck.h
@@ -0,0 +1,14 @@
+//
+//  PlayingCardDeck.h
+//  cardGame
+//
+//  Created by Wei on 8/8/15.
+//  Copyright (c) 2015 Gate Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "Deck.h"
+
+@interface PlayingCardDeck : Deck
+
+@end
---
+++ cardGame/cardGame/PlayingCardDeck.m
@@ -0,0 +1,32 @@
+//
+//  PlayingCardDeck.m
+//  cardGame
+//
+//  Created by Wei on 8/8/15.
+//  Copyright (c) 2015 Gate Inc. All rights reserved.
+//
+
+#import "PlayingCardDeck.h"
+#import "PlayingCard.h"
+
+@implementation PlayingCardDeck
+
+- (instancetype) init
+{
+    self = [super init];
+    
+    if (self) {
+        for (NSString *suit in [PlayingCard validSuits]) {
+            for (NSUInteger rank = 1; rank <= [PlayingCard maxRank]; rank++) {
+                PlayingCard *card = [[PlayingCard alloc] init];
+                card.rank = rank;
+                card.suit = suit;
+                [self addCard:card];
+            }
+        }
+    }
+    
+    return self;
+}
+
+@end
