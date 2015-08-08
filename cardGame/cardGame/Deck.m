6e61c7040234a1fe2cd56ab59cfbc018032916b4
Wei Xia <xw900812@gmail.com>
2015-08-08 17:56:45 -0400

    Lecture 2

--- cardGame/cardGame/Deck.m
+++ cardGame/cardGame/Deck.m
@@ -9,9 +9,25 @@
 #import "Deck.h"
 
 @interface  Deck()
-
+@property (strong, nonatomic) NSMutableArray *cards;
 @end
 
 @implementation Deck
+
+- (void)addCard:(Card *)card atTop:(BOOL)atTop
+{
+    if (atTop) {
+        [self.cards insertObject:card atIndex:0];
+    } else {
+        [self addCard:card atTop:NO];
+    }
+}
+
+- (void)addCard:(Card *)card
+{
+    [self addCard:card atTop:NO];
+}
+
+- (Card *)drawRandomCard {}
 
 @end
