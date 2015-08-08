8551cd6df91af103af007224616dac30c296159c
Wei Xia <xw900812@gmail.com>
2015-08-08 18:02:31 -0400

    Lecture 2

---
+++ cardGame/cardGame/PlayingCard.h
@@ -0,0 +1,14 @@
+//
+//  PlayingCard.h
+//  cardGame
+//
+//  Created by Wei on 8/8/15.
+//  Copyright (c) 2015 Gate Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "Card.h"
+
+@interface PlayingCard : Card
+
+@end
---
+++ cardGame/cardGame/PlayingCard.m
@@ -0,0 +1,13 @@
+//
+//  PlayingCard.m
+//  cardGame
+//
+//  Created by Wei on 8/8/15.
+//  Copyright (c) 2015 Gate Inc. All rights reserved.
+//
+
+#import "PlayingCard.h"
+
+@implementation PlayingCard
+
+@end
