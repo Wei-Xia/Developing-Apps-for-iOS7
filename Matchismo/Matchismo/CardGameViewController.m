31f4e9140b126ca02a20485c6f29de3ef7a1e945
Wei Xia <xw900812@gmail.com>
2015-08-10 23:13:32 -0400

    Lecture 2

--- Matchismo/Matchismo/CardGameViewController.m
+++ Matchismo/Matchismo/CardGameViewController.m
@@ -14,7 +14,21 @@
 
 @implementation CardGameViewController
 
-- (IBAction)touchCardButton:(id)sender {
+- (IBAction)touchCardButton:(UIButton *)sender
+{
+    if ([sender.currentTitle length]) {
+        UIImage *cardImage = [UIImage imageNamed:@"cardback"];
+        [sender setBackgroundImage:cardImage
+                          forState:UIControlStateNormal];
+        [sender setTitle:@"" forState:UIControlStateNormal];
+    } else{
+        UIImage *cardImage = [UIImage imageNamed:@"cardfront"];
+        [sender setBackgroundImage:cardImage
+                          forState:UIControlStateNormal];
+        [sender setTitle:@"" forState:UIControlStateNormal];
+    }
+    
+
 }
 
 
