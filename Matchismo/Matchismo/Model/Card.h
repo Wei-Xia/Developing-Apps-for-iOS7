def94bfa63205842edc5c352c88a2244fbf58040
Wei Xia <xw900812@gmail.com>
2015-08-10 23:38:07 -0400

    Lecture 2

---
+++ Matchismo/Matchismo/Model/Card.h
@@ -0,0 +1,13 @@
+//
+//  Card.h
+//  Matchismo
+//
+//  Created by Wei on 8/10/15.
+//  Copyright (c) 2015 CS193p. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Card : NSObject
+
+@end
---
+++ Matchismo/Matchismo/Model/Card.m
@@ -0,0 +1,13 @@
+//
+//  Card.m
+//  Matchismo
+//
+//  Created by Wei on 8/10/15.
+//  Copyright (c) 2015 CS193p. All rights reserved.
+//
+
+#import "Card.h"
+
+@implementation Card
+
+@end
