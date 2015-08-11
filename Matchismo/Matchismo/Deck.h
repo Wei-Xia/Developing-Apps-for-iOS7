a836f196558fa2d438060065a6a0d1006efb96b9
Wei Xia <xw900812@gmail.com>
2015-08-10 23:44:53 -0400

    Lecture 2

---
+++ Matchismo/Matchismo/Deck.h
@@ -0,0 +1,13 @@
+//
+//  Deck.h
+//  Matchismo
+//
+//  Created by Wei on 8/10/15.
+//  Copyright (c) 2015 CS193p. All rights reserved.
+//
+
+#import "Card.h"
+
+@interface Deck : Card
+
+@end
---
+++ Matchismo/Matchismo/Deck.m
@@ -0,0 +1,13 @@
+//
+//  Deck.m
+//  Matchismo
+//
+//  Created by Wei on 8/10/15.
+//  Copyright (c) 2015 CS193p. All rights reserved.
+//
+
+#import "Deck.h"
+
+@implementation Deck
+
+@end
